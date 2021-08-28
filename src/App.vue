9e4be23c391f99a4b99f04a987e9682b453658c7
ldl <919578094@qq.com>
2021-08-28 21:49:53 +0800

    navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="app">
-
+    <Navigation />
   </div>
 </template>
 
 <script>
-
+import Navigation from './components/Navigation';
 export default {
   name: 'App',
   components: {
+    Navigation
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+// app.vue的css没有scope，所以大家都可以运用到这里面的样式
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+.link-light {
+  color: #fff;
 }
 </style>
